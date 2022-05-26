4e4c1cb4fb8e2d5cc9769d13cdfd8210b542f9e3
AayushiGandhi <ay753882@dal.ca>
2022-05-26 11:47:45 -0300

    Edit and delete only for the post created

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -37,11 +37,15 @@ const Post = ({ post, setCurrentId }) => {
                 <Typography variant="h6">{post.name}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
-            <div className={classes.overlay2}>
-                <Button style={{color:'white'}} size="small" onClick={() => setCurrentId(post._id)}>
-                    <MoreHorizIcon fontSize="medium" />
-                </Button>
-            </div>
+
+            {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+                <div className={classes.overlay2}>
+                    <Button style={{color:'white'}} size="small" onClick={() => setCurrentId(post._id)}>
+                        <MoreHorizIcon fontSize="medium" />
+                    </Button>
+                </div>
+            )}
+            
             <div className={classes.details}>
                 <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
             </div>
@@ -55,10 +59,13 @@ const Post = ({ post, setCurrentId }) => {
                 <Button color='primary' disabled={!user?.result} size="small" onClick={() => {dispatch(likePost(post._id))}}>
                     <Likes />
                 </Button>
-                <Button color='primary' size="small" onClick={() => dispatch(deletePost(post._id))}>
-                    <DeleteIcon fontSize="small" />
-                    Delete
-                </Button>
+
+                {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+                    <Button color='primary' size="small" onClick={() => dispatch(deletePost(post._id))}>
+                        <DeleteIcon fontSize="small" />
+                        Delete
+                    </Button>
+                )}
             </CardActions>
         </Card>
     );
